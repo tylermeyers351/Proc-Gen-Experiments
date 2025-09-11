ea61e8e98d7f4238a0578076d845e2dcd86eaa60
tylermeyers351 <tylermeyers351@gmail.com>
2025-09-11 10:25:46 -0700

    Added navmesh logic, transparent walls, player animation.

---
+++ Assets/Scripts/DirectedAgent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DirectedAgent : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void MoveToLocation(Vector3 targetPoint)
+    {
+        agent.destination = targetPoint;
+        agent.isStopped = false;
+    }
+
+    void Update()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
+    }
+}
